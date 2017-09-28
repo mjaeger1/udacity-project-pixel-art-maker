b26ed90fc316a600038052686990bfe3342c4ea8
Markus Jaeger <jaeger.markus@gmx.at>
2017-09-28 22:26:30 +0200

    Added height/width event listening

--- designs.js
+++ designs.js
@@ -3,8 +3,19 @@
 
 // When size is submitted by the user, call makeGrid()
 
+let gridHeight, gridWidth, gridColor;
+
+$('#sizePicker').submit(function(){
+  gridHeight = $('#input_height').val();
+  gridWidth = $('#input_width').val();
+  console.log(gridHeight +"/" + gridWidth);
+  makeGrid(gridHeight,gridWidth);
+});
+
+
 function makeGrid() {
-
-// Your code goes here!
-
+  console.log("makeGridFunction");
+  for (var i = 0; i < gridHeight; i++) {
+    $('#pixel_canvas').append("<tr></tr>");
+  }
 }
