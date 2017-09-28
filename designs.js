173408b4043e100416ddf8dd42c2de3e1c906980
Markus Jaeger <jaeger.markus@gmx.at>
2017-09-28 23:04:52 +0200

    Added makeGrid()

--- designs.js
+++ designs.js
@@ -5,17 +5,33 @@
 
 let gridHeight, gridWidth, gridColor;
 
+// NOTE TO MYSELF: non-jquery way
+// document.getElementsByTagName("form")[0].onsubmit = function() {... return false;}
+
 $('#sizePicker').submit(function(){
   gridHeight = $('#input_height').val();
   gridWidth = $('#input_width').val();
-  console.log(gridHeight +"/" + gridWidth);
   makeGrid(gridHeight,gridWidth);
+  return false; //needed to avoid form reset
 });
 
 
 function makeGrid() {
-  console.log("makeGridFunction");
+
+  $('tr').remove();
+  $('td').remove();
+
   for (var i = 0; i < gridHeight; i++) {
     $('#pixel_canvas').append("<tr></tr>");
   }
+
+  var colHtml = "";
+
+  for (var j = 0; j < gridWidth; j++) {
+    colHtml += "<td></td>"
+  }
+
+  $('tr').each(function(){
+    $(this).html(colHtml);
+  });
 }
